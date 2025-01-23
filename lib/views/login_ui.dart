bddcfdd0308aadafed80a1428968b96168b6edc4
Phiraphat29 <s6652410029@sau.ac.th>
2025-01-23 13:33:39 +0700

    Complete

--- lib/views/login_ui.dart
+++ lib/views/login_ui.dart
@@ -14,154 +14,161 @@ class _LoginUIState extends State<LoginUI> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-        child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Icon(
-                  Icons.arrow_back_ios_new,
-                  size: MediaQuery.of(context).size.height * 0.05,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+          child: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Image.asset('assets/images/logo.png',
-                    height: MediaQuery.of(context).size.height * 0.2),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Welcome Back, ",
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.height * 0.035,
-                    fontWeight: FontWeight.bold,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Icon(
+                      Icons.arrow_back_ios_new,
+                      size: MediaQuery.of(context).size.height * 0.05,
+                    ),
                   ),
                 ),
-              ),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  "Make it work, make it right, make it fast.",
-                  style: TextStyle(
-                    fontSize: MediaQuery.of(context).size.height * 0.02,
-                    fontWeight: FontWeight.bold,
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Image.asset('assets/images/logo.png',
+                      height: MediaQuery.of(context).size.height * 0.2),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Welcome Back, ",
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.035,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.03,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.person),
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  labelText: 'Email',
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Make it work, make it right, make it fast.",
+                    style: TextStyle(
+                      fontSize: MediaQuery.of(context).size.height * 0.02,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  prefixIcon: Icon(Icons.fingerprint),
-                  suffixIcon: Icon(Icons.visibility),
-                  enabledBorder: OutlineInputBorder(),
-                  focusedBorder: OutlineInputBorder(),
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.03,
                 ),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
+                TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.person),
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.fingerprint),
+                    suffixIcon: Icon(Icons.visibility),
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text("Forgot Password?",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue)),
+                  ),
+                ),
+                ElevatedButton(
                   onPressed: () {},
-                  child: Text("Forgot Password?",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.blue)),
+                  child: Text(
+                    "LOGIN",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      fixedSize: Size(MediaQuery.of(context).size.width,
+                          MediaQuery.of(context).size.height * 0.065),
+                      backgroundColor: Colors.black),
                 ),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "LOGIN",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
                 ),
-                style: ElevatedButton.styleFrom(
+                Text("OR"),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.01,
+                ),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/images/googlelogo.png",
+                        width: 20.0,
+                      ),
+                      Text(
+                        " Sign-In with Google",
+                      )
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5.0),
                     ),
                     fixedSize: Size(MediaQuery.of(context).size.width,
                         MediaQuery.of(context).size.height * 0.065),
-                    backgroundColor: Colors.black),
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              Text("OR"),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.01,
-              ),
-              OutlinedButton(
-                onPressed: () {},
-                child: Row(
+                  ),
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Image.asset(
-                      "assets/images/googlelogo.png",
-                      width: 20.0,
-                    ),
                     Text(
-                      " Sign-In with Google",
+                      "Don't have an Account?",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignupUI(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Sign Up",
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 33, 83, 232),
+                            fontWeight: FontWeight.bold),
+                      ),
                     )
                   ],
-                ),
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0),
-                  ),
-                  fixedSize: Size(MediaQuery.of(context).size.width,
-                      MediaQuery.of(context).size.height * 0.065),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't have an Account?",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignupUI(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      "Sign Up",
-                      style: TextStyle(
-                          color: Color.fromARGB(255, 33, 83, 232),
-                          fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
-              )
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/views/welcome_ui.dart
+++ lib/views/welcome_ui.dart
@@ -16,84 +16,86 @@ class _WelcomeUIState extends State<WelcomeUI> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.amber,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.25,
-            ),
-            Image.asset(
-              'assets/images/logo.png',
-              width: MediaQuery.of(context).size.width * 0.65,
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * 0.125,
-            ),
-            Text(
-              "DTI SAU APP 2020",
-              style: TextStyle(
-                fontSize: MediaQuery.of(context).size.height * 0.03,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.25,
               ),
-            ),
-            Text("Southeast Asia University",
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-              "Created by Phiraphat Loratsachan",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(
-              height: MediaQuery.of(context).size.width * 0.125,
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-              OutlinedButton(
-                onPressed: () {
-                  // เปิด Login UI แบบย้อนกลับได้
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => LoginUI(),
-                    ),
-                  );
-                },
-                child: Text('LOGIN',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, color: Colors.black)),
-                style: OutlinedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0)),
-                  fixedSize: Size(MediaQuery.of(context).size.width * 0.25,
-                      MediaQuery.of(context).size.height * 0.05),
-                ),
+              Image.asset(
+                'assets/images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.65,
               ),
               SizedBox(
-                width: MediaQuery.of(context).size.width * 0.03,
+                height: MediaQuery.of(context).size.height * 0.125,
               ),
-              ElevatedButton(
-                onPressed: () {
-                  // เปิด SignUp UI แบบย้อนกลับได้
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignupUI(),
-                    ),
-                  );
-                },
-                child: Text(
-                  "SIGNUP",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold, color: Colors.white),
+              Text(
+                "DTI SAU APP 2020",
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.height * 0.03,
+                  fontWeight: FontWeight.bold,
                 ),
-                style: ElevatedButton.styleFrom(
+              ),
+              Text("Southeast Asia University",
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                "Created by Phiraphat Loratsachan",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.width * 0.125,
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                OutlinedButton(
+                  onPressed: () {
+                    // เปิด Login UI แบบย้อนกลับได้
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => LoginUI(),
+                      ),
+                    );
+                  },
+                  child: Text('LOGIN',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.black)),
+                  style: OutlinedButton.styleFrom(
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    ),
+                        borderRadius: BorderRadius.circular(5.0)),
                     fixedSize: Size(MediaQuery.of(context).size.width * 0.25,
                         MediaQuery.of(context).size.height * 0.05),
-                    backgroundColor: Colors.black),
-              )
-            ])
-          ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.03,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    // เปิด SignUp UI แบบย้อนกลับได้
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignupUI(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "SIGNUP",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      fixedSize: Size(MediaQuery.of(context).size.width * 0.25,
+                          MediaQuery.of(context).size.height * 0.05),
+                      backgroundColor: Colors.black),
+                )
+              ])
+            ],
+          ),
         ),
       ),
     );
